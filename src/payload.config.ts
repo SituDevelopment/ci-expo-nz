4dc789d4fb6e6f281951a76c5c44ffd2919e44dc
Sassiest01 <nthom.1@outlook.com>
2025-02-04 01:06:51 +0000

    Run prettier and add dependencies

---
+++ src/payload.config.ts
@@ -0,0 +1,48 @@
+// storage-adapter-import-placeholder
+import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
+import path from "path";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+import { fileURLToPath } from "url";
+
+import { Media } from "./collections/Media";
+import { Users } from "./collections/Users";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+    admin: {
+        user: Users.slug,
+        importMap: {
+            baseDir: path.resolve(dirname),
+        },
+    },
+    collections: [Users, Media],
+    editor: lexicalEditor(),
+    secret: process.env.PAYLOAD_SECRET || "",
+    typescript: {
+        outputFile: path.resolve(dirname, "payload-types.ts"),
+    },
+    db: vercelPostgresAdapter({
+        pool: {
+            connectionString: process.env.POSTGRES_URL || "",
+        },
+    }),
+    sharp,
+    plugins: [
+        payloadCloudPlugin(),
+        vercelBlobStorage({
+            enabled: true, // Optional, defaults to true
+            // Specify which collections should use Vercel Blob
+            collections: {
+                media: true,
+            },
+            // Token provided by Vercel once Blob storage is added to your Vercel project
+            token: process.env.BLOB_READ_WRITE_TOKEN,
+        }),
+    ],
+});
